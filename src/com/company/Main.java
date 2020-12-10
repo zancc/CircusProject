7f705bde3dcd90afa4791182adf50cb73fb8d65f
Zane Kaibe <zane.kaibe@gmail.com>
2020-12-10 23:55:56 +0200

    Main methods are done, game board actions started

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static Scanner scanner = new Scanner (System.in);
-
+    public static ArrayList<Player> players = new ArrayList<>();
 
     public static void main(String[] args) {
         String name;
@@ -14,26 +15,75 @@ public class Main {
         System.out.println("Choose the number of players (1 or 2):");
         int numberOfPlayer = inputNumbOfPlayers();
         System.out.println("Number of players = " + numberOfPlayer);
-        scanner.nextLine();
 
-        System.out.println("Enter the name of the first player: ");
-        name = scanner.nextLine();
-        System.out.println("Enter first players token in form of one symbol");
-        token = inputToken();
-        Player player1 = new Player(name, token);
 
-        Player player2 = null;
-        if (numberOfPlayer == 2) {
-            System.out.println("Enter the name of the second player: ");
+        for (int i = 0; i < numberOfPlayer; i++) {
+            System.out.println("Enter the name of the player number "+ (i + 1) +  ": ");
             name = scanner.nextLine();
-            System.out.println("Enter second players token in form of one symbol");
+            System.out.println("Enter " + name + "'s chosen token in form of one symbol");
             token = inputToken();
-            player2 = new Player(name, token);
+            players.add(new Player(name, token));
+            System.out.println(players.get(i));
         }
 
-        System.out.println(player1);
-        System.out.println(player2);
+        int diceValue = 0;
+        boolean endOfGame = false;
 
+        while(!endOfGame) {
+
+            for (int i = 0; i < numberOfPlayer; i++) {
+                Player currentPlayer = players.get(i);
+                System.out.println("Enter the value of the dice thrown (1, 2, 3, 4, 5 or 6) by player " + currentPlayer.getName());
+                diceValue = readDiceValue(); //reads and checks the dice value for player 1
+                System.out.println(diceValue);
+
+                currentPlayer.addToPosition(diceValue); //adding the value of the dice to the position of player 1
+                System.out.println(currentPlayer.getName() + "Players position immediately after throwing the dice: "
+                        + currentPlayer.getPosition()); //prints out players1 position
+
+                currentPlayer.boardAction(currentPlayer.getPosition());
+                System.out.println(currentPlayer.getName() + "Players position after the action: " + currentPlayer.getPosition());
+
+
+
+
+                // if player crosses the 120 mark - then needs to go back
+                if (currentPlayer.getPosition() > 120) {
+                    int pos = currentPlayer.getPosition();
+                    currentPlayer.setPosition(120 - (pos-120));
+                    System.out.println(currentPlayer.getName() + " player crossed the end and returned back, current position: "
+                            + currentPlayer.getPosition());
+
+                }
+
+
+
+                if (players.get(i).getPosition() == 120) {
+                    System.out.println("Game over. The winner is " + currentPlayer.getName());
+                    endOfGame = true;
+                }
+            }
+        }
+    }
+
+    private static int readDiceValue() {
+        boolean correctInput = false;
+        int diceValue = 0;
+        while (!correctInput) {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Enter the value of the dice thrown (1, 2, 3, 4, 5 or 6):");
+                scanner.nextLine();
+            }
+            diceValue = scanner.nextInt();
+            if ( !(diceValue >= 1 && diceValue <= 6)) {
+                System.out.println("Dice value must be 1, 2, 3, 4, 5 or 6:");
+                correctInput = false;
+            } else {
+                correctInput = true;
+                return diceValue;
+            }
+        }
+        return diceValue;
     }
 
     private static char inputToken() {
@@ -60,6 +110,7 @@ public class Main {
                 scanner.nextLine();
             }
             numberOfPlayers = scanner.nextInt();
+            scanner.nextLine();
             if ( !(numberOfPlayers == 1 || numberOfPlayers == 2)) {
                 System.out.println("Number of players must be 1 or 2:");
                 correctInput = false;
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -27,6 +27,10 @@ public class Player {
         this.position = position;
     }
 
+    public void addToPosition(int position) {
+        this.position += position;
+    }
+
     @Override
     public String toString() {
         return "Player{" +
@@ -34,4 +38,34 @@ public class Player {
                 ", token=" + token +
                 '}';
     }
+
+    public String boardAction(int fieldNumber) {
+        String action = "";
+
+        switch (fieldNumber) {
+            case 3:
+                action = "Kangaroo with a phone - go forward to the field No.19";
+                this.setPosition(19);
+                break;
+            case 9:
+                action = "Seal with a ball - go forward to the field No.11";
+                this.setPosition(11);
+                break;
+            case 14:
+                action = "Bear - tightrope walker - go forward to the field No.30";
+                this.setPosition(30);
+                break;
+            case 16:
+                action = "Girl - tightrope dancer - go back to the first field";
+                this.setPosition(1);
+                break;
+
+            default:
+                action = "This is regular field without special action";
+                break;
+        }
+        System.out.println(action);
+        return action;
+    }
+
 }
